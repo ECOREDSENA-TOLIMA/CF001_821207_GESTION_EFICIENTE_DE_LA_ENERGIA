87df9b91c8356248c531d2629716b5fbf8d6df1a
Oscar Espitia <odespitia@sena.edu.co>
2023-07-20 12:32:28 -0500

    ajustes cf1

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -11,11 +11,11 @@
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
           h2 Símbolos eléctricos
-        p.mb-4 #[b Objetivo] Relacionar los principales símbolos utilizados en el contexto de la según el Ministerio de Minas y Energía.
+        p.mb-4 #[b Objetivo] Relacionar los principales símbolos utilizados en el contexto de la electricidad según el Ministerio de Minas y Energía.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Arreastrar y soltar
+              p.fw-bold.mb-0 Arrastrar y soltar
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
                 span Realizar
